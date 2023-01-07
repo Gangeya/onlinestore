15da774c047773f618ddcf97490e5108d74f1c2e
Farkhod Kodirov <farxod94@gmail.com>
2023-01-08 03:58:40 +0500

    feat: add product page

--- src/core/components/header/index.ts
+++ src/core/components/header/index.ts
@@ -11,16 +11,20 @@ class Header extends BaseComponent {
     const logo=new BaseComponent('div').setClass('logo').render(div_header);
     new BaseComponent('img').setAttribute('src', logoimg).setAttribute('width', '200').render(logo);
     const cartblock=new BaseComponent('div').setClass('cart-block').render(div_header);
-    new BaseComponent('div').setClass('total-price').setContent('100 $').render(cartblock);
+    const totalincart=new BaseComponent('div').setClass('total-in-cart').render(cartblock);
+    new BaseComponent('span').setContent('Итого в корзине: ').render(cartblock);
+    new BaseComponent('span').setClass('total-price').setContent('1 258 $').render(cartblock);
     const cart=new BaseComponent('div').setClass('cart').render(cartblock);
     // new BaseComponent('div').setClass('total-price').setContent('100 $').render(this);
-    new BaseComponent('img').setAttribute('src', cartimg).setAttribute('width', '32').setAttribute('heigth', '32').render(cart);
+    const cartlink=new BaseComponent('a').setClass('cart-link').setAttribute('href','#cart').render(cart);
+    new BaseComponent('img').setAttribute('src', cartimg).setAttribute('width', '32').setAttribute('heigth', '32').render(cartlink);
     new BaseComponent('span').setClass('cart-count').setContent('2').render(cart);
     const bread=new BaseComponent('div').setClass('breadcrumbs').render(this);
     const breadul=new BaseComponent('ul').setClass('breadcrumbs-list').render(bread);
     const limain=new BaseComponent('li').render(breadul);
     new BaseComponent('a').setAttribute('href','#main').setContent('Main').render(limain);
-    new BaseComponent('li').setContent('Phone').render(breadul);
+    const product=new BaseComponent('li').render(breadul);
+    new BaseComponent('a').setAttribute('href','#product').setContent('Product').render(product);
   } 
   
 }
